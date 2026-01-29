15edd1f094798f7f0e3cb778a463a0e1d64c189d
WillyxWonka <kalif.edu@gmail.com>
2026-01-29 13:08:28 -0800

    added pizza javascript

---
+++ pizza.js
@@ -0,0 +1,40 @@
+const doc =document.getElementById("pizza-form");
+
+doc.onsubmit = () => {
+    clearErrors();
+    let isValid = true;
+    
+    let fName = document.getElementById("fname").value.trim();
+    let lName = document.getElementById("lname").value.trim();
+    let Email = document.getElementById("email").value.trim();
+
+    let pickUp = document.getElementById("PickUp");
+    let delivery = document.getElementById("delivery");
+    
+    if(!fName){
+        document.getElementById("err-fname").style.display = "block" ;
+        isValid =false;
+    }
+    if(!lName){
+        document.getElementById("err-lname").style.display = "block";
+        isValid =false;
+    }
+    if(!Email){
+        document.getElementById("err-email").style.display = "block";
+        isValid =false;
+    }
+    if(!pickUp.checked && !delivery.checked){
+        document.getElementById("err-method").style.display = "block";
+        isValid = false;
+    }
+    return isValid;
+}
+
+function clearErrors(){
+    let errors = document.getElementsByClassName("err");
+    for(er of errors){
+        er.style.display = "none"
+    }
+}
+
+
