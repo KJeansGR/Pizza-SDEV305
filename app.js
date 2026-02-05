bf1774f08b7f3ae2ef335b85cfc8f96cb2cd653d
WillyxWonka <kalif.edu@gmail.com>
2026-02-05 14:26:34 -0800

    added app commands like get and post from form, added routes

--- app.js
+++ app.js
@@ -7,11 +7,61 @@ const app = express();
 //define aport number where the servervwill listen
 const PORT = 3000;
 
+//MiddleWare that allows express 
+// //to read form data and store it in req.body
+app.use(express.urlencoded({extended: true}))
+//create temp array to store orders
+const orders = [];
+
+
 //define our main root ('/')
 app.get('/', (req,res)=>{
 
     res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
+
+//Contact Route
+app.get('/contact-us', (req,res)=>{
+
+    res.sendFile(`${import.meta.dirname}/views/contact.html`);
+});
+
+//Thankyou for order
+app.post('/submit-order', (req,res)=>{
+
+    //store data
+    const order ={
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        method: req.body.method,
+        toppings: req.body.toppings || "none",
+        size:  req.body.size,
+        timestamp : new Date()
+    };
+
+    orders.push(order);
+    //res.send(orders); // displays the json of orders array
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+//admin route
+app.get('/admin', (req, res)=>{
+
+    res.send(orders); // displays the json of orders array
+
+})
+
+
+
+
+//Thankyou for order
+app.get('/thank-you', (req,res)=>{
+
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+
 
 //start server, and listen on designated PORT
 app.listen(PORT, ()=>{
